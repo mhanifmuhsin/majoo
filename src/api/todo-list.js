32e09feb179e6af61c9d7142a75732ed9e408d0b
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2022-05-24 09:45:52 +0700

    :hammer:Create .env

--- src/api/todo-list.js
+++ src/api/todo-list.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+import {config} from '../config';
 
 export async function getTodoList() {
-  return await axios.get(
-    'https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list',
+  return await axios.get(`${config.api_host}/to-do-list`,
     {}
   );
 }
