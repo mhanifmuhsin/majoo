c16d4f31981ba3da9eec3d1e272006935cecec6a
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2022-05-24 11:28:10 +0700

    :hammer:Disable delete for status 1

--- src/components/ModalDetail.jsx
+++ src/components/ModalDetail.jsx
@@ -65,6 +65,7 @@ export default function ModalDetail({ isOpen, closeModal , todo, handleDelete}) 
                       type="button"
                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={()=>handleDelete(todo.id)}
+                      disabled={todo?.status===1}
                     >
                       Delete
                     </button>
